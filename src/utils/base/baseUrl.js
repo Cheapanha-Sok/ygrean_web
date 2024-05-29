9e6925df6a77ebc9bd33a835d8531acea7126f5c
panha <sokcheapanha11@gmail.com>
2024-05-29 14:10:01 +0700

    redeploy

--- src/utils/base/baseUrl.js
+++ src/utils/base/baseUrl.js
@@ -1,2 +1,1 @@
-// export const REACT_APP_API_ENDPOINT="https://api.ygrean.site"
-export const REACT_APP_API_ENDPOINT="http://localhost:8000"
+export const REACT_APP_API_ENDPOINT="https://api.ygrean.site"
