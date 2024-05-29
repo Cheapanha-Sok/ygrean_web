39574c1f33f0cdc81534f695c711e03458420697
panha <sokcheapanha11@gmail.com>
2024-05-29 15:47:09 +0700

    remove dist

--- src/page/user/quiz/components/Choice.jsx
+++ src/page/user/quiz/components/Choice.jsx
@@ -3,7 +3,9 @@ import Input from "../../../../ui/shared/Input";
 export default function Choice({ data, onSelect }) {
   return (
     <li className="flex items-center gap-4 p-3 bg-gray-100 rounded-md hover:bg-gray-200 transition-colors cursor-pointer">
-      <label htmlFor={data.id} className="text-lg font-semibold text-gray-700">{data.name}</label>
+      <label htmlFor={data.id} className="text-lg font-semibold text-gray-700">
+        {data.name}
+      </label>
       <Input
         type="radio"
         name="choice"
