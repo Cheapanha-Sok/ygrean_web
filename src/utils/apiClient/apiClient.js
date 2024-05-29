cadb04bb4ee1200bdf309fb4a76e759d0f3d3005
panha <sokcheapanha11@gmail.com>
2024-05-29 08:13:52 +0700

    new deploy

--- src/utils/apiClient/apiClient.js
+++ src/utils/apiClient/apiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const { REACT_APP_API_ENDPOINT } = process.env;
+import { REACT_APP_API_ENDPOINT } from "../base/baseUrl";
 
 const apiClient = axios.create({
   baseURL: REACT_APP_API_ENDPOINT,
