8afd30b66cce6ae625a2a95f8e6604fd0da8e3d4
panha <sokcheapanha11@gmail.com>
2024-05-29 08:12:55 +0700

    remove dist

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env.REACT_APP_API_ENDPOINT": JSON.stringify(
-        env.REACT_APP_API_ENDPOINT
-      ),
-    },
-    plugins: [react()],
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
